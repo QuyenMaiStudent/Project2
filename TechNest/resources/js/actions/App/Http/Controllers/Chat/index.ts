f56832a6e51e9ea0b297edbacd6a190dee9c2d28
QuyenMaiStudent <quyenmxg.124010124083@vtc.edu.vn>
2025-12-03 20:52:38 +0700

    Sửa lại thư mục

--- TechNest/resources/js/actions/App/Http/Controllers/Chat/index.ts
+++
@@ -1,8 +0,0 @@
-import ChatBotController from './ChatBotController'
-import ChatController from './ChatController'
-const Chat = {
-    ChatBotController: Object.assign(ChatBotController, ChatBotController),
-ChatController: Object.assign(ChatController, ChatController),
-}
-
-export default Chat
